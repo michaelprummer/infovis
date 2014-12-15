e8868f97865341c06d99fcac3688407567802d53
michaelprummer <mp@phexmedia.de>
2014-12-15 22:14:19 +0100

    php loader gibt jetzt nur noch wichtige Infos geparsed zurück

--- Loader.php
+++ Loader.php
@@ -15,18 +15,43 @@ class Loader {
             for ($i=0; $i<count($this->result); $i++) {
                 $val = $this->result[$i];
 
-                    if (strpos($val, "year_separator") !== false) {
-                        if($block_counter > 0)
-                            echo "</div>";
+                // YEAR BLOCK
+                if (strpos($val, "year_separator") !== false) {
+                    if($block_counter > 0)
+                        echo "</div>";
 
-                        $block_counter++;
-                        $val = strip_tags($val);
-                        echo "<div data-name='$val' class='block'>";
-                        echo  "<h1 class='year'>" . $val . "</h1>";
+                    $block_counter++;
+                    $val = strip_tags($val);
+                    echo "<div data-name='$val' class='block'>";
+                    echo  "<h1 class='year'>" . $val . "</h1>";
 
-                    } elseif (strpos($val, "<td>") !== false) {
-                        echo "<div class='element'>" . $val . "</div>";
+                // ELEMENT BLOCK
+                } elseif (strpos($val, "<td>") !== false) {
+                    echo "<div class='element'>";
+                    $val = explode("\n", $val);
+
+                    for ($k=0; $k<count($val); $k++) {
+                        if($k == 2) {
+                            echo "<div class='ele-autoren'>" . $val[$k] . "</div>";
+
+                        } elseif ($k == 4) {
+                            echo "<div class='ele-title'>" . strip_tags($val[$k], "<a>") . "</div>";
+
+                        } elseif ($k == 6) {
+                            echo "<div class='ele-details'>" . strip_tags($val[$k]) . "</div>";
+
+                        } elseif ($k == 7) {
+                            preg_match('/<a href="(.+)">/', $val[$k], $match);
+                            if(count($match) > 0)
+                                echo "<div class='ele-bib-link'>" . $match[1] . "</div>";
+                            else
+                                echo "<div class='ele-bib-link'></div>";
+                        }
+
                     }
+
+                    echo "</div>";
+                }
             }
 
             // Close year block
@@ -34,9 +59,6 @@ class Loader {
                 echo "</div>";
             }
 
-
-                //print_r($this->result);
-                //echo $this->result;
         } else {
             echo "<p class='error'>Error loading data from: $url</p>";
         }
