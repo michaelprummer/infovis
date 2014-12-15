ed13e9905237a808055f56a075e5c96d2c1eb14f
michaelprummer <mp@phexmedia.de>
2014-12-15 23:43:11 +0100

    ajax loader animation

--- js/parser.js
+++ js/parser.js
@@ -5,20 +5,32 @@
 
 Parser = function($param){
     var url = "ajax.php";
+    var isDone = false;
 
-    $.ajax({
-        url: url,
-        type: 'post',
-        data: {
-            "param0": $param[0],
-            "param1": $param[1],
-            "param2": $param[2],
-            "param3": $param[3],
-            "param4": $param[4]
-        }
-    }).done(function (data){
-        $('body').append(data);
-    })
+
+    $(document).ready(function() {
+        $("body").prepend("<div id='ajaxloader'></div>")
+
+        $.ajax({
+            url: url,
+            type: 'post',
+            data: {
+                "param0": $param[0],
+                "param1": $param[1],
+                "param2": $param[2],
+                "param3": $param[3],
+                "param4": $param[4]
+            }
+        }).done(function (data){
+            $('body').append(data);
+
+            $('#ajaxloader').animate({
+                opacity: 0
+            }, 1000, "linear", function() {
+                $('#ajax-loader').remove()
+            });
+        })
+    });
 
 }
 
