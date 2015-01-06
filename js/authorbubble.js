5e9b83d43ad8b7b04482b2d0d84b152e7f4fd956
michaelprummer <mp@phexmedia.de>
2015-01-06 17:33:19 +0100

    dynamic bubble size

--- js/authorbubble.js
+++ js/authorbubble.js
@@ -12,23 +12,21 @@ AuthorBubble = function(options){
     this.yearPaperCount = [];
     this.paperTitles = [];
     this.detailView = true;
-    var that = this;
-
     this.width = $("#svg-container").attr("width");
     this.height = $("#svg-container").attr("height");
+    var that = this;
 
-
-    AuthorBubble.prototype.showDetails = function(){
+    AuthorBubble.prototype.showDetails = function() {
         this.detailView = !this.detailView;
-        if(this.detailView == false){
+        if(this.detailView == false) {
             this.paperFan.transition().style("opacity",0);
             this.activityPie.transition().style("opacity",0);
-        }else{
+        } else {
             this.paperFan.transition().style("opacity",1);
             this.activityPie.transition().style("opacity",1);
         }
-
     }
+
     AuthorBubble.prototype.listPapers = function(){
         ret = [];
         for (var i = 0; i < this.papers.length; i++) {
@@ -46,41 +44,34 @@ AuthorBubble = function(options){
             this.yearPaperCount.push(this.papers[i].elements.length);
         }
         this.paperTitles = this.listPapers();
-
     }
+
+    Number.prototype.clamp = function(min, max) {
+        return Math.min(Math.max(this, min), max);
+    };
+
     //console.log(this.yearPaperCount);
-
     this.createD3Data();
-
-//    console.log("creating author bubble with name: "+this.authorname);
     this.bubble = this.svg.append("g").attr("transform", "translate(" + 0 + "," + 0 + ")").attr("class","authorBubble");
-
 
     // ACTIVITY PIE
     this.activityPie =  this.bubble.append("g").attr("class","activityPie");
     var monochrome = d3.scale.ordinal().range(["#01EC6A","#1DF47D","#60FDA6","#91FDC1","#CEFEE3","#F7FEFA"]);
-        var w = 300;
-        var h = 300;
-        var outerRadius = w / 2;
-        var innerRadius = 100;
-        var color= monochrome;
+    this.innerRadius = (this.paperTitles.length*1.5).clamp(40,200);
+    this.outerRadius =  this.innerRadius +50;
+    var color= monochrome;
+    var arc = d3.svg.arc()
+        .innerRadius(this.innerRadius)
+        .outerRadius(this.outerRadius);
 
-        var arc = d3.svg.arc()
-            .innerRadius(innerRadius)
-            .outerRadius(outerRadius);
+    var pie = d3.layout.pie().value(function(d){return d;});
 
-        var pie = d3.layout.pie().value(function(d){return d;});
-
-
-        //Set up groups
-        var arcs = this.activityPie.selectAll("g.activityArc")
-            .data(
-                pie(that.yearPaperCount)
-            )
-            .enter()
-            .append("g")
+    //Set up groups
+    var arcs = this.activityPie.selectAll("g.activityArc")
+        .data(pie(that.yearPaperCount))
+            .enter().append("g")
             .attr("class", "activityArc")
-            .attr("transform", "translate(" + (this.width/2 - innerRadius) + "," + (this.width/2 - innerRadius) + ")");
+            .attr("transform", "translate(" + (this.width/2) + "," + (this.height/2) + ")");
 
         //Draw arc paths
         arcs.append("path")
@@ -92,39 +83,39 @@ AuthorBubble = function(options){
             return color(i);
         });
 
-        //year Labels
-        arcs.append("text")
-            .attr("transform", function(d) {
-                return "translate(" + arc.centroid(d) + ")";
-            })
-            .attr("text-anchor", "middle")
-            .text(function(d,i) {
-                return that.yearLabels[i];
-            });
+    //year Labels
+    arcs.append("text")
+        .attr("transform", function(d) {
+            return "translate(" + arc.centroid(d) + ")";
+        })
+        .attr("text-anchor", "middle")
+        .text(function(d,i) {
+            return that.yearLabels[i];
+        });
 
 
     // AUTHOR NAME =>CLICKABLE?
-        var namebadge = this.bubble
-            .append("g")
-            .attr("class","name")
-            .on("mousedown",function(){
-                that.showDetails();
-            });
+    var namebadge = this.bubble
+        .append("g")
+        .attr("class","name")
+        .on("mousedown",function(){
+            that.showDetails();
+        });
 
-        namebadge.append("circle")
-            .style("fill", "green")
-            .attr("cx",this.width/2 - innerRadius)
-            .attr("cy",this.height/2 - innerRadius)
-            .attr("r",innerRadius)
-            .attr("text-anchor", "middle")
+    namebadge.append("circle")
+        .style("fill", "green")
+        .attr("cx",this.width/2)
+        .attr("cy",this.height/2)
+        .attr("r",this.innerRadius)
+        .attr("text-anchor", "middle")
 
-        namebadge.append("text")
-            .text(this.authorname)
-            .attr("text-anchor", "middle")
-            .attr("dx",this.width/2 - innerRadius)
-            .attr("dy",this.height/2 - innerRadius)
-            .attr("class","authorname")
-            .style("fill","#ffffff");
+    namebadge.append("text")
+        .text(this.authorname)
+        .attr("text-anchor", "middle")
+        .attr("dx",this.width/2)
+        .attr("dy",this.height/2)
+        .attr("class","authorname")
+        .style("fill","#ffffff");
 
 
     //PAPERS not working
@@ -143,9 +134,9 @@ AuthorBubble = function(options){
         .attr("transform", function(d, i) {
             var angle = (360/that.paperTitles.length)*i-85;
             this.currentAngle = angle;
-            var r = 240;
-            var cx = (that.width/2 - 100);
-            var cy = (that.height/2 - 100);
+            var r = that.outerRadius + 100;
+            var cx = (that.width/2);
+            var cy = (that.height/2);
             var x = cx + r *Math.cos(angle*0.0174532925);
             var y = cy + r *Math.sin(angle*0.0174532925);
             return "translate(" + x + ", "  + y +") rotate(" + angle + ")";
@@ -177,8 +168,6 @@ AuthorBubble = function(options){
 
                     return text;
                 })
-
-
         })
         .style("font-size","11.5pt")
         .attr("text-anchor", "middle")
